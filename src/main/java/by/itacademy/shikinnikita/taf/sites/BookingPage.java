0f6611453e2ba27d33939219532941c2d50e6a8d
nikitashikin <so7nem@gmail.com>
2023-05-25 14:22:39 +0300

    add Booking Page and Tests

---
+++ src/main/java/by/itacademy/shikinnikita/taf/sites/BookingPage.java
@@ -0,0 +1,12 @@
+package by.itacademy.shikinnikita.taf.sites;
+
+public class BookingPage {
+
+    public String buttonEnterSelector = "#b2indexPage > div.c85f9f100b.cb6c8dd99f > div > div > div > div.dd2db21cc4 > div > a";
+    public String inputEmailXpath = "//*[@id='username']";
+    public String buttonContinueWithEmailXpath = "//div/form/div[3]/button/span";
+    public String inputPasswordSelector = "#password";
+    public String buttonSubmitXpath = "//*[@id='root']/div/div/div/div[2]/div[1]/div/div/div/div/div/div/form/div[2]/button";
+
+}
+
---
+++ src/test/java/by/itacademy/shikinnikita/taf/sites/BookingTest.java
@@ -0,0 +1,116 @@
+package by.itacademy.nikitashikin.taf.sites;
+
+import by.itacademy.shikinnikita.taf.sites.BookingPage;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class BookingTest {
+    @Test
+    public void testWithEmptyEmail() throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        BookingPage bookingPage = new BookingPage();
+        driver.get("https://www.booking.com/");
+
+        Thread.sleep(2000);
+        WebElement buttonEnter = driver.findElement(By.cssSelector(bookingPage.buttonEnterSelector));
+        buttonEnter.click();
+
+
+        WebElement inputEmail = driver.findElement(By.xpath(bookingPage.inputEmailXpath));
+        inputEmail.sendKeys("");
+
+        WebElement buttonContinueWithEmail = driver.findElement(By.xpath(bookingPage.buttonContinueWithEmailXpath));
+        driver.quit();
+
+    }
+
+    @Test
+    public void testWithIncorrectEmail() throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        BookingPage bookingPage = new BookingPage();
+        driver.get("https://www.booking.com/");
+
+        Thread.sleep(2000);
+        WebElement buttonEnter = driver.findElement(By.cssSelector(bookingPage.buttonEnterSelector));
+        buttonEnter.click();
+
+
+        WebElement inputEmail = driver.findElement(By.xpath(bookingPage.inputEmailXpath));
+        inputEmail.sendKeys("mail");
+
+        WebElement buttonContinueWithEmail = driver.findElement(By.xpath(bookingPage.buttonContinueWithEmailXpath));
+        driver.quit();
+
+    }
+    @Test
+    public void testWithCorrectEmail() throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        BookingPage bookingPage = new BookingPage();
+        driver.get("https://www.booking.com/");
+
+        Thread.sleep(2000);
+        WebElement buttonEnter = driver.findElement(By.cssSelector(bookingPage.buttonEnterSelector));
+        buttonEnter.click();
+
+
+        WebElement inputEmail = driver.findElement(By.xpath(bookingPage.inputEmailXpath));
+        inputEmail.sendKeys("test@mail.com");
+
+        WebElement buttonContinueWithEmail = driver.findElement(By.xpath(bookingPage.buttonContinueWithEmailXpath));
+        buttonContinueWithEmail.click();
+        driver.quit();
+    }
+    @Test
+    public void testWithEmptyPassword() throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        BookingPage bookingPage = new BookingPage();
+        driver.get("https://www.booking.com/");
+
+        Thread.sleep(2000);
+        WebElement buttonEnter = driver.findElement(By.cssSelector(bookingPage.buttonEnterSelector));
+        buttonEnter.click();
+
+
+        WebElement inputEmail = driver.findElement(By.xpath(bookingPage.inputEmailXpath));
+        inputEmail.sendKeys("test@mail.com");
+
+        WebElement buttonContinueWithEmail = driver.findElement(By.xpath(bookingPage.buttonContinueWithEmailXpath));
+        buttonContinueWithEmail.click();
+
+        Thread.sleep(2000);
+        WebElement buttonSubmit = driver.findElement(By.xpath(bookingPage.buttonSubmitXpath));
+        buttonSubmit.click();
+
+        driver.quit();
+    }
+    @Test
+    public void testWithAnyPassword() throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        BookingPage bookingPage = new BookingPage();
+        driver.get("https://www.booking.com/");
+
+        Thread.sleep(2000);
+        WebElement buttonEnter = driver.findElement(By.cssSelector(bookingPage.buttonEnterSelector));
+        buttonEnter.click();
+
+
+        WebElement inputEmail = driver.findElement(By.xpath(bookingPage.inputEmailXpath));
+        inputEmail.sendKeys("test@mail.com");
+
+        WebElement buttonContinueWithEmail = driver.findElement(By.xpath(bookingPage.buttonContinueWithEmailXpath));
+        buttonContinueWithEmail.click();
+
+        Thread.sleep(2000);
+        WebElement inputPassword = driver.findElement(By.cssSelector(bookingPage.inputPasswordSelector));
+        inputPassword.sendKeys("123");
+
+        WebElement buttonSubmit = driver.findElement(By.xpath(bookingPage.buttonSubmitXpath));
+        buttonSubmit.click();
+
+        driver.quit();
+
+    }
+}
